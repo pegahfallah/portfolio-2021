dc7b0d5ac757cdb4f90abdccc4b3737b4749fb0e
Pegah <43738935+pegahfallah@users.noreply.github.com>
2021-04-02 13:52:44 -0400

    added route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Switch, Route } from "react-router";
+import HomePage from "./pages/HomePage";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>Pegah Fallah</h1>
+      <p>This site is under construction</p>
+    </>
+  );
+}
